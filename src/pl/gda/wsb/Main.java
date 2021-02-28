54cb9450c06d9bba670d8a11f073fe021b97f7a2
Mateusz Sosnowski <msosnowski@wsb.gda.pl>
2021-02-28 10:38:25 +0100

    Add class: Animal, Phone, Human

--- src/pl/gda/wsb/Main.java
+++ src/pl/gda/wsb/Main.java
@@ -4,5 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Human human1 = new Human("Jan","Nowak","Java Developer");
+        Human human2 = new Human("Jan","Kowalski","Automation Tester");
+
+        Animal animal1 = new Animal("mouse","Jerry",0.9);
+        Animal animal2 = new Animal("lion","Mufasa",123.6);
+
+        Phone phone1 = new Phone("SAMSUNG","S10",9.1);
+        Phone phone2 = new Phone("NOKIA","5.1",8.2);
+
+        System.out.println(human1);
+        System.out.println(human1.firstName);
     }
 }
---
+++ src/pl/gda/wsb/Phone.java
@@ -0,0 +1,13 @@
+package pl.gda.wsb;
+
+public class Phone {
+    String producer;
+    String model;
+    Double screenSize;
+
+    public Phone(String producer, String model, Double screenSize) {
+        this.producer = producer;
+        this.model = model;
+        this.screenSize = screenSize;
+    }
+}
