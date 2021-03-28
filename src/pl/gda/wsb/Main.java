fd525e9edbef8bf3c934bd3e8d93396abf66b1f6
Mateusz Sosnowski <msosnowski@wsb.gda.pl>
2021-03-28 15:54:21 +0200

    zadanie 4

--- src/pl/gda/wsb/Main.java
+++ src/pl/gda/wsb/Main.java
@@ -1,5 +1,7 @@
 package pl.gda.wsb;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,6 +9,7 @@ public class Main {
 
         Human human1 = new Human("Jan", "Nowak", "Java Developer");
         Human human2 = new Human("Jan", "Kowalski", "Automation Tester");
+        Human human3 = new Human("Piotr", "Nowy", "Support man");
 
         Animal animal1 = new Animal("mouse", "Jerry", 0.9);
         Animal animal2 = new Animal("lion", "Mufasa", 123.6);
@@ -20,5 +23,18 @@ public class Main {
         if (animal1.weight > animal2.weight)
             System.out.println("Cięższy jest " + animal1.name + ". Jego waga: " + animal1.weight);
         else System.out.println("Cięższy jest " + animal2.name + ". Jego waga: " + animal2.weight);
+
+        System.out.println("\n---------------------\n");
+
+        ArrayList<Human> humansList = new ArrayList<>();
+        humansList.add(human1);
+        humansList.add(human2);
+        humansList.add(human3);
+
+        for (Human human : humansList){
+            System.out.println(human.lastName);
+        }
+
+
     }
 }
