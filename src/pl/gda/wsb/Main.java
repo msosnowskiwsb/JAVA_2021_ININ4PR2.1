43f04da635d4dc69975623c2353402c32547bb49
Mateusz Sosnowski <msosnowski@wsb.gda.pl>
2021-03-28 15:03:50 +0200

    zadanie 3

--- src/pl/gda/wsb/Main.java
+++ src/pl/gda/wsb/Main.java
@@ -5,16 +5,20 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        Human human1 = new Human("Jan","Nowak","Java Developer");
-        Human human2 = new Human("Jan","Kowalski","Automation Tester");
+        Human human1 = new Human("Jan", "Nowak", "Java Developer");
+        Human human2 = new Human("Jan", "Kowalski", "Automation Tester");
 
-        Animal animal1 = new Animal("mouse","Jerry",0.9);
-        Animal animal2 = new Animal("lion","Mufasa",123.6);
+        Animal animal1 = new Animal("mouse", "Jerry", 0.9);
+        Animal animal2 = new Animal("lion", "Mufasa", 123.6);
 
-        Phone phone1 = new Phone("SAMSUNG","S10",9.1);
-        Phone phone2 = new Phone("NOKIA","5.1",8.2);
+        Phone phone1 = new Phone("SAMSUNG", "S10", 9.1);
+        Phone phone2 = new Phone("NOKIA", "5.1", 8.2);
 
         System.out.println(human1);
         System.out.println("Human: " + human1.firstName + " " + human1.lastName + " position: " + human1.position);
+
+        if (animal1.weight > animal2.weight)
+            System.out.println("Cięższy jest " + animal1.name + ". Jego waga: " + animal1.weight);
+        else System.out.println("Cięższy jest " + animal2.name + ". Jego waga: " + animal2.weight);
     }
 }
